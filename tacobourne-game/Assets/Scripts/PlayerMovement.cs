2ea0f6b33616fb6ef837003c37e6bf06df831c88
NSGOmega <70560728+NSGOmega@users.noreply.github.com>
2022-03-10 14:37:00 -0500

    Updated PlayerMovement.cs
    Player direction now changes depending on if A or D is pressed.
    
    Script now detects for objects tagged with "Ground"

--- tacobourne-game/Assets/Scripts/PlayerMovement.cs
+++ tacobourne-game/Assets/Scripts/PlayerMovement.cs
@@ -1,39 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed;
-    public float jumpForce;
+    [SerializeField] private float speed;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
+    float someScale;
+
+    private void Awake()
+    {
+        //Grabs references for rigidbody and animator from game object.
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        someScale = -transform.localScale.x;
     }
 
     private void Update()
     {
+        float movement = Input.GetAxis("Horizontal");
 
-        /*
-         * Horizontal movement controller
-         * Returns 1 or -1 depending on if A or D is pressed
-        */
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
 
-        var movement = Input.GetAxis("Horizontal"); 
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+        //Flip player when facing left/right.
+        if (movement > 0.01f)
+            transform.localScale = new Vector2(someScale, transform.localScale.y);
+        else if (movement < -0.01f)
+            transform.localScale = new Vector2(-someScale, transform.localScale.y);
 
-        /*
-         * Vetical movement controller
-         * If jump key pressed and y position is 0 add force to rigidbody
-        */
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f) 
-        {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        }
+        //sets animation parameters
+        //anim.SetBool("run", horizontalInput != 0);
+        //anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
